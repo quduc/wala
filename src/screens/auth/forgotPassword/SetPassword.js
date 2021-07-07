bbf3841ac85b63f71f036b6bb18311187fd4a354
đức anh trần <ducanh131297@gmail.com>
2021-07-07 16:30:45 +0700

    push code

--- src/screens/auth/forgotPassword/SetPassword.js
+++ src/screens/auth/forgotPassword/SetPassword.js
@@ -1,44 +1,45 @@
-import React, { useRef, useEffect } from 'react';
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { useFormik } from 'formik';
+import React, { useRef, useEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { useFormik } from "formik";
 
-import { Body, Text, Button, Password, Header } from '@components/index';
-import * as screenTypes from '@navigation/screenTypes';
-import { useDispatch, useSelector } from 'react-redux';
-import { setPassword } from '@modules/auth/slice';
+import { Body, Text, Button, Password, Header } from "@components/index";
+import * as screenTypes from "@navigation/screenTypes";
+import { useDispatch, useSelector } from "react-redux";
+import { setPassword } from "@modules/auth/slice";
 
-import { setPasswordLoadingSelector } from '@modules/auth/selectors';
+import { setPasswordLoadingSelector } from "@modules/auth/selectors";
 
-import { isPassword } from '@utils/index';
-import { useModal } from '@common/customHook';
-import { useTranslation } from 'react-i18next';
-import i18next from 'i18next';
-import SvgComponent from '@assets/svg';
+import { isPassword } from "@utils/index";
+import { useModal } from "@common/customHook";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+import SvgComponent from "@assets/svg";
+import Toast from "react-native-toast-message";
 
-const validate = values => {
+const validate = (values) => {
   const errors = {};
 
   if (!values.password) {
-    errors.password = i18next.t('message:MSG_2', {
-      field: i18next.t('common:password'),
+    errors.password = i18next.t("message:MSG_2", {
+      field: i18next.t("common:password"),
     });
   } else if (!isPassword(values.password)) {
-    errors.password = i18next.t('message:MSG_4');
+    errors.password = i18next.t("message:MSG_4");
   }
 
   if (!values.rePassword) {
-    errors.rePassword = i18next.t('message:MSG_2', {
-      field: i18next.t('common:password'),
+    errors.rePassword = i18next.t("message:MSG_2", {
+      field: i18next.t("common:password"),
     });
   } else if (values.rePassword !== values.password) {
-    errors.rePassword = i18next.t('message:MSG_9');
+    errors.rePassword = i18next.t("message:MSG_9");
   }
 
   return errors;
 };
 
 const Login = () => {
-  const { t } = useTranslation(['auth', 'common']);
+  const { t } = useTranslation(["auth", "common"]);
   const navigation = useNavigation();
   const dispatch = useDispatch();
   const route = useRoute();
@@ -56,7 +57,7 @@ const Login = () => {
   const rePasswordRef = useRef(null);
 
   const formik = useFormik({
-    initialValues: { password: '', rePassword: '' },
+    initialValues: { password: "", rePassword: "" },
     validate,
   });
 
@@ -73,18 +74,23 @@ const Login = () => {
           password: formik.values.password,
         },
         onSuccess: () => {
-          modal.success({
-            content: t('txt_set_password_success'),
-            okButton: () => navigation.navigate(screenTypes.LoginScreen),
+          Toast.show({
+            type: "success",
+            props: {
+              message: t("txt_set_password_success"),
+              onClose: () => navigation.navigate(screenTypes.LoginScreen),
+            },
           });
         },
-        onError: e => {
-          modal.error({
-            title: t('common:title_error'),
-            content: e.errorMessage,
+        onError: (e) => {
+          Toast.show({
+            type: "error",
+            props: {
+              message: e.errorMessage,
+            },
           });
         },
-      }),
+      })
     );
   };
 
@@ -104,32 +110,32 @@ const Login = () => {
         isBack={false}
         iconLeft={SvgComponent.back}
         onLeftPress={goToRequestOtp}
-        title={t('txt_header_set_password_screen')}
+        title={t("txt_header_set_password_screen")}
       />
       <Text mt={50} mb={24}>
-        {t('txt_title_in_set_password_screen')}
+        {t("txt_title_in_set_password_screen")}
       </Text>
 
       <Password
         mt={18}
         selectTextOnFocus
         onSubmitEditing={onFocusRePassword}
-        placeholder={t('placeholder_new_password')}
-        onBlur={formik.handleBlur('password')}
+        placeholder={t("placeholder_new_password")}
+        onBlur={formik.handleBlur("password")}
         value={formik.values.password}
-        onChangeText={formik.handleChange('password')}
+        onChangeText={formik.handleChange("password")}
         error={formik.errors.password && formik.touched.password}
         errorMessage={formik.errors.password}
       />
 
       <Password
         mt={18}
-        placeholder={t('placeholder_re_password')}
+        placeholder={t("placeholder_re_password")}
         ref={rePasswordRef}
         selectTextOnFocus
-        onBlur={formik.handleBlur('rePassword')}
+        onBlur={formik.handleBlur("rePassword")}
         value={formik.values.rePassword}
-        onChangeText={formik.handleChange('rePassword')}
+        onChangeText={formik.handleChange("rePassword")}
         error={formik.errors.rePassword && formik.touched.rePassword}
         errorMessage={formik.errors.rePassword}
       />
@@ -140,9 +146,10 @@ const Login = () => {
         gradient
         borderRadius={3}
         disabled={isDisableButton()}
-        onPress={onSetPassword}>
+        onPress={onSetPassword}
+      >
         <Text c1 medium>
-          {t('txt_btn_reset_pass')}
+          {t("txt_btn_reset_pass")}
         </Text>
       </Button>
       {contextHolder}
