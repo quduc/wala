41a7466766c4dbe42ecac2c5953cf87ec39ce7e3
đức anh trần <ducanh131297@gmail.com>
2021-08-01 15:25:42 +0700

    push code

--- ios/Pods/Target Support Files/Pods-EasyChat-EasyChatTests/Pods-EasyChat-EasyChatTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EasyChat_EasyChatTests : NSObject
-@end
-@implementation PodsDummy_Pods_EasyChat_EasyChatTests
-@end
---
+++ ios/Pods/Target Support Files/Pods-Wala/Pods-Wala-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wala : NSObject
+@end
+@implementation PodsDummy_Pods_Wala
+@end
