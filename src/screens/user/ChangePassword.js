b205961cd050fa09638fc3cd8564912646e6b267
đức anh trần <ducanh131297@gmail.com>
2021-08-11 22:07:07 +0700

    push code

--- src/screens/user/ChangePassword.js
+++ src/screens/user/ChangePassword.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/order */
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Body,
   Block,
@@ -7,48 +7,48 @@ import {
   Button,
   Touchable,
   Password,
-} from '@components/index';
-import colors from '@assets/colors';
-import HeaderProfile from '../../components/header/HeaderProfile';
-import { useTranslation } from 'react-i18next';
-import * as screenTypes from '@navigation/screenTypes';
-import { useNavigation } from '@react-navigation/core';
-import { useFormik } from 'formik';
-import { updatePasswordApi } from '@modules/profile/services';
-import i18next from 'i18next';
-import { isPassword } from '@utils/';
-import { useModal } from '@common/customHook';
+} from "@components/index";
+import colors from "@assets/colors";
+import HeaderProfile from "../../components/header/HeaderProfile";
+import { useTranslation } from "react-i18next";
+import * as screenTypes from "@navigation/screenTypes";
+import { useNavigation } from "@react-navigation/core";
+import { useFormik } from "formik";
+import { updatePasswordApi } from "@modules/profile/services";
+import i18next from "i18next";
+import { isPassword } from "@utils/";
+import { useModal } from "@common/customHook";
 
-const validate = values => {
+const validate = (values) => {
   const errors = {};
   if (!values.currentPassword.trim()) {
-    errors.currentPassword = i18next.t('message:MSG_2', {
-      field: i18next.t('common:password'),
+    errors.currentPassword = i18next.t("message:MSG_2", {
+      field: i18next.t("common:password"),
     });
   } else if (!isPassword(values.currentPassword)) {
-    errors.currentPassword = i18next.t('message:MSG_4');
+    errors.currentPassword = i18next.t("message:MSG_4");
   }
 
   if (!values.newPassword.trim()) {
-    errors.newPassword = i18next.t('message:MSG_2', {
-      field: i18next.t('common:password'),
+    errors.newPassword = i18next.t("message:MSG_2", {
+      field: i18next.t("common:password"),
     });
   } else if (!isPassword(values.newPassword)) {
-    errors.newPassword = i18next.t('message:MSG_4');
+    errors.newPassword = i18next.t("message:MSG_4");
   } else if (values.newPassword === values.currentPassword) {
-    errors.newPassword = i18next.t('message:MSG_41');
+    errors.newPassword = i18next.t("message:MSG_41");
   }
 
   if (values.newPassword !== values.confirmNewPassword) {
-    errors.confirmNewPassword = i18next.t('message:MSG_2', {
-      field: i18next.t('common:password'),
+    errors.confirmNewPassword = i18next.t("message:MSG_2", {
+      field: i18next.t("common:password"),
     });
   }
   return errors;
 };
 
 export default function ChangePassword() {
-  const { t } = useTranslation(['translation', 'auth', 'message']);
+  const { t } = useTranslation(["translation", "auth", "message"]);
   const navigation = useNavigation();
   const [loading, setLoading] = useState(false);
   const [modal, contextHolder] = useModal();
@@ -62,9 +62,9 @@ export default function ChangePassword() {
 
   const formik = useFormik({
     initialValues: {
-      currentPassword: '',
-      newPassword: '',
-      confirmNewPassword: '',
+      currentPassword: "",
+      newPassword: "",
+      confirmNewPassword: "",
     },
     validate,
   });
@@ -75,11 +75,11 @@ export default function ChangePassword() {
       setLoading(true);
       await updatePasswordApi(
         formik.values.currentPassword,
-        formik.values.confirmNewPassword,
+        formik.values.confirmNewPassword
       );
       setTimeout(() => {
         modal.success({
-          content: t('auth:txt_set_password_success'),
+          content: t("auth:txt_set_password_success"),
           okButton: () => {
             goBack();
           },
@@ -88,7 +88,7 @@ export default function ChangePassword() {
     } catch (error) {
       setTimeout(() => {
         modal.error({
-          title: t('common:title_error'),
+          title: t("common:title_error"),
           content: error.errorMessage,
         });
       }, 300);
@@ -111,20 +111,20 @@ export default function ChangePassword() {
     <>
       <Body ph={16} loading={loading} keyboardAvoid>
         <HeaderProfile
-          title={t('translation:changePassword')}
+          title={t("translation:changePassword")}
           onPressRight={onChangePassword}
         />
         <Block mt={16} center>
           <Text c1 extraBold>
-            {t('currentPassword')}
+            {t("currentPassword")}
           </Text>
           <Password
             mt={8}
             color={colors.white}
-            placeholder={t('plsTypeCurrentPassword')}
+            placeholder={t("plsTypeCurrentPassword")}
             value={formik.values.currentPassword}
-            onChangeText={formik.handleChange('currentPassword')}
-            onBlur={e => handleTrimWhenBlurInput('currentPassword', e)}
+            onChangeText={formik.handleChange("currentPassword")}
+            onBlur={(e) => handleTrimWhenBlurInput("currentPassword", e)}
             error={
               formik.errors.currentPassword && formik.touched.currentPassword
             }
@@ -133,30 +133,30 @@ export default function ChangePassword() {
         </Block>
         <Block mt={16} center>
           <Text c1 extraBold>
-            {t('newPassword')}
+            {t("newPassword")}
           </Text>
           <Password
             mt={8}
             color={colors.white}
-            placeholder={t('plsTypeNewPassword')}
+            placeholder={t("plsTypeNewPassword")}
             value={formik.values.newPassword}
-            onChangeText={formik.handleChange('newPassword')}
-            onBlur={e => handleTrimWhenBlurInput('newPassword', e)}
+            onChangeText={formik.handleChange("newPassword")}
+            onBlur={(e) => handleTrimWhenBlurInput("newPassword", e)}
             error={formik.errors.newPassword && formik.touched.newPassword}
             errorMessage={formik.errors.newPassword}
           />
         </Block>
         <Block mt={16} center>
           <Text c1 extraBold>
-            {t('confirmNewPassword')}
+            {t("confirmNewPassword")}
           </Text>
           <Password
             mt={8}
             color={colors.white}
-            placeholder={t('PlsRetypeNewPassword')}
+            placeholder={t("PlsRetypeNewPassword")}
             value={formik.values.confirmNewPassword}
-            onBlur={e => handleTrimWhenBlurInput('confirmNewPassword', e)}
-            onChangeText={formik.handleChange('confirmNewPassword')}
+            onBlur={(e) => handleTrimWhenBlurInput("confirmNewPassword", e)}
+            onChangeText={formik.handleChange("confirmNewPassword")}
             error={
               formik.errors.confirmNewPassword &&
               formik.touched.confirmNewPassword
@@ -170,16 +170,12 @@ export default function ChangePassword() {
           mt={16}
           pv={8}
           borderRadius={3}
-          onPress={onChangePassword}>
+          onPress={onChangePassword}
+        >
           <Text c1 medium>
-            {t('changePassword')}
+            {t("changePassword")}
           </Text>
         </Button>
-        <Touchable middle onPress={goToForgotPassword}>
-          <Text medium c1 color={colors.orange} mt={16}>
-            {t('forgotPassword?')}
-          </Text>
-        </Touchable>
       </Body>
       {contextHolder}
     </>
